af91b7ce6cdfee74894debdd6e6f3f4047f7af07
allisonbusse <allisonbusse@gmail.com>
2021-03-12 16:43:51 -0800

    add current line

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,9 +3,10 @@ import styles from './AboutMe.css';
 
 const AboutMe = () => (
   <section className={styles.AboutMe}>
-    <img src='https://raw.githubusercontent.com/allisonbusse/portfolio/master/src/assets/IMG_9447.JPG'/>
+    <img src='https://raw.githubusercontent.com/allisonbusse/portfolio/master/src/assets/IMG_9447.JPG' />
     <div className={styles.bio}>
-      <p>Technical writer turned developer. Logical thinker. 
+      <p><span className={styles.five}><strong>Currently: Front-end Software Engineer at HealthSparq</strong></span>
+        <br /> <br />Technical writer turned developer. Logical thinker.
         <br /><span className={styles.two}>Creative problem-solver. Efficiency aficionado.</span>
         <br /><span className={styles.three}>Fan of simple, intuitive design and clear code.</span>
         <br /><span className={styles.four}>Pair coding enthusiast. Crossword nerd.</span>
